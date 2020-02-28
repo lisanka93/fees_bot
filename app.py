6dc6971216c3996728aaa8816202e1bcba36b1f2
lisanka93 <lisa.chalaguine@gmail.com>
2020-02-28 14:04:36 +0000

    added concern bot

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import numpy as np
 import pickle
 import os
+from nltk.stem import WordNetLemmatizer
 cwd = os.getcwd()
 
 
@@ -33,7 +34,7 @@ chat_logs = {}
 # stores start and end time for each user
 timestamps = {}
 
-pickle_path = cwd + "/chatlogs/"
+pickle_path = cwd + "/chatlogs2/"
 
 
 """
@@ -57,6 +58,10 @@ with open(stop_words_file, "r") as f:
         stop_words.extend(line.split())
 #print(stop_words)
 
+stop_words = stop_words + ['education', 'university', 'uni', 'universities', 'fee', 'fees', 'abolish', 'abolished', 'people', 'students', 'student', 'degree', 'tuition']
+#df_lisa
+
+
 
 def preprocess(raw_text):
 
@@ -70,7 +75,8 @@ def preprocess(raw_text):
     cleaned_words = []
 
     for word in words:
-        #word = lemmatizer.lemmatize(word)
+        lemmatizer = WordNetLemmatizer()
+        word = lemmatizer.lemmatize(word)
         if word not in stop_words:
             cleaned_words.append(word)
 
@@ -100,6 +106,47 @@ def cosine_distance_wordembedding_method(s1, s2):
         return round((cosine)*100,2)
     except:
         return 0
+
+
+"""
+****************************************************************
+CONCERN CLASSIFICATION
+****************************************************************
+"""
+
+
+def get_top_k_predictions_(model,X_test,k):
+
+    # get probabilities instead of predicted labels, since we want to collect top 3
+    probs = model.predict_proba(X_test)
+
+    prob_list = list(probs[0])
+    prob_list.sort(reverse=True)
+
+    # GET TOP K PREDICTIONS BY PROB - note these are just index
+    best_n = np.argsort(probs, axis=1)[:,-k:]
+
+    # GET CATEGORY OF PREDICTIONS
+    preds=[[model.classes_[predicted_cat] for predicted_cat in prediction] for prediction in best_n]
+
+    preds=[ item[::-1] for item in preds]
+
+    pred_prob = prob_list[:k]
+
+    return preds, pred_prob
+
+
+filename = 'finalized_model.sav'
+
+filename2 = "transformer.sav"
+
+# load the model from disk
+concern_model = pickle.load(open(filename, 'rb'))
+transformer = pickle.load(open(filename2, 'rb'))
+
+with open('concern_dic.pickle', 'rb') as handle:
+    concern_dic = pickle.load(handle)
+
 
 
 """
@@ -134,30 +181,35 @@ SIMILARITY MEASUREMENT AND ARGUMENT RETURN
 ****************************************************************
 """
 def most_similar(argument, argument_list=con_arguments):
-
+    most_sim_args = []
     for arg in argument_list:
         #print(arg[0])
         sim = cosine_distance_wordembedding_method(arg[0], argument)
 
         if sim < 100 and sim >= 95:
-            print('most sim 95+: ', sim, arg[0])
-            return arg[1]
+            #print('most sim 95+: ', sim, arg[1])
+            most_sim_args.append(arg[1])
 
 
     #second iteration if no match found
+
     for arg in argument_list:
         #print(arg[0])
         sim = cosine_distance_wordembedding_method(arg[0], argument)
         #print(sim)
-        if sim >= 90:
-            print('most sim 90: ',sim, arg[0])
-            return arg[1]
+        if sim >= 93:
+            #print('most sim 90: ',sim, arg[0])
+            most_sim_args.append(arg[1])
+
+    if len(most_sim_args) > 0:
+        return most_sim_args
 
     return "no arg"
 
-def return_arg(user_mes):
+def return_arg_and_concern(user_mes):
 
     response_id = 0
+    concern = "no concern"
     user_mes = user_mes.lower()
 
     # checking first whether person agrees - then no need to check for counterarg
@@ -170,16 +222,25 @@ def return_arg(user_mes):
 
     if bool_agree == True and bool_disagree == False and len(user_mes.split()) < 6:
         chatbot_reply = "no arg"
-        return(response_id, chatbot_reply)
+        return(response_id, chatbot_reply, response_id)
 
     #then checking whether message is very short - then ask to expand
     if len(user_mes.split()) < 6:
         chatbot_reply = "Why? Please expand"
-        return(response_id, chatbot_reply)
+        return(response_id, chatbot_reply, response_id)
 
 
     # now lets preprocess and look for a match in the KB
     message_prep = preprocess(user_mes)
+    message_sen = sen = ' '.join(message_prep) # as string for classifier
+    message_features = transformer.transform([message_sen])
+    concerns_, preds = get_top_k_predictions_(concern_model, message_features, 2)
+    #print(concerns_, preds)
+
+    if preds[1] >= 0.3:
+        concerns = concerns_[0]   #list
+    else:
+        concerns = [concerns_[0][0]]
 
     message_model = []
 
@@ -193,25 +254,58 @@ def return_arg(user_mes):
             #continue
     #id of most similar argument in KB
     most_sim_id = most_similar(message_model) # returns no arg if no match found where cosine similarity above 90
+    #print("ids of similar", most_sim_id)
 
 
     if most_sim_id == "no arg":
         chatbot_reply = "no arg"
-        return(response_id, chatbot_reply)
+        #print("no similar found")
+        return(response_id, chatbot_reply, response_id)
     else:
         try:
+            concern_1 = concerns[0]
+            print(concern_1)
+            #most sim id is now a list of most similar arguments
+            #we are looping through them
+            for arg in most_sim_id:
+                possible_responses = con_dic[arg]
+                #print("possible responses", possible_responses)
+                for pr in possible_responses:
+                    CA_concerns = concern_dic[pr]
+                    #print("CA concerns", CA_concerns)
+                    if concern_1 in CA_concerns:
+                        possible_responses.remove(pr)
+                        con_dic[arg] = possible_responses
+                        chatbot_reply = pro_dic[pr]
+                        #print("response id", )
+                        return(pr, chatbot_reply, arg)
+
+            if len(concerns) > 1:
+                concern_2 = concerns[1]
+
+                for arg in most_sim_id:
+                    possible_responses = con_dic[arg]
+                    for pr in possible_responses:
+                        CA_concerns = concern_dic[pr]
+                        if concern_2 in CA_concerns:
+                            possible_responses.remove(pr)
+                            con_dic[most_sim_id] = possible_responses
+                            chatbot_reply = pro_dic[pr]
+                            return(pr, chatbot_reply, arg)
+
+
             #since an argument has several CAs in the KB eventually the list might be empty - hence exception
             # it would probably be smart to then go back and find another similar match but whatever - can fix that later
-            possible_responses = con_dic[most_sim_id]
-            con_dic[most_sim_id] = con_dic[most_sim_id][1:]
-            response_id = possible_responses[0]
-            chatbot_reply = pro_dic[response_id]
+            #possible_responses = con_dic[most_sim_id]
+            #con_dic[most_sim_id] = con_dic[most_sim_id][1:]
+            #response_id = possible_responses[0]
+            #chatbot_reply = pro_dic[response_id]
         except:
             chatbot_reply = "no arg"
-            return(response_id, chatbot_reply)
+            return(response_id, chatbot_reply, response_id)
 
-
-    return(most_sim_id, chatbot_reply)
+        chatbot_reply = "no arg"
+    return(response_id, chatbot_reply, response_id) #RESPONSE ID!!!
 
 
 
@@ -241,6 +335,7 @@ def receive_message():
     else:
 
         output = request.get_json()
+        #print(concern_dic)
 
 
         recipient_id = output['entry'][0]['messaging'][0]['sender']['id']  #unicode, should i typecast it into string or int? lets see...
@@ -284,7 +379,7 @@ def receive_message():
             return "ok"
 
         elif stop == "stop":
-            response_sent_text = "You are ending the chat. Please go to the google form to complete the study: https://forms.gle/aqDtpUKcd2Xrd3A69"
+            response_sent_text = "You are ending the chat. Please go to the google form to complete the study: https://forms.gle/of9n7nfyZ3GabDru8"
             send_message(recipient_id, response_sent_text)
 
             log_mes= "User: " + user_mes
@@ -303,7 +398,7 @@ def receive_message():
 
 
         else:
-            response_id, response_sent_text = return_arg(user_mes)
+            response_id, response_sent_text, sim_id = return_arg_and_concern(user_mes)
 
             if response_sent_text == "no arg":
                 try:
@@ -313,7 +408,7 @@ def receive_message():
 
                 except:
 
-                    response_sent_text = "I ran out of arguments :) please go to the google form to complete the study: https://forms.gle/aqDtpUKcd2Xrd3A69"
+                    response_sent_text = "I ran out of arguments :) please go to the google form to complete the study: https://forms.gle/of9n7nfyZ3GabDru8"
                     send_message(recipient_id, response_sent_text)
                     timestamps[recipient_id].append(mes_time)
                     #adding last argument where no match was found to chatlog
@@ -333,7 +428,7 @@ def receive_message():
 
 
             send_message(recipient_id, response_sent_text)
-            log_mes= "User: " + user_mes
+            log_mes= "User: " + user_mes + " " + str(sim_id)
             chat_logs[recipient_id].append(log_mes)
             if response_id == 0:
                 id_ = str(len(user_ids_dic[recipient_id]))
